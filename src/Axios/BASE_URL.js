22e03b7b96aeb17b3c055aecbe3a7b4a035f099a
jennifer <jennlee0929@gmail.com>
2021-12-16 19:28:19 -0800

    changed the default domain to prod

--- src/Axios/BASE_URL.js
+++ src/Axios/BASE_URL.js
@@ -1,8 +1,8 @@
 const NETLIFY_FUNCTIONS = ".netlify/functions/api";
 
-// const BASE_URL = "https://callisto-server.netlify.app/" + NETLIFY_FUNCTIONS;
+const BASE_URL = "https://callisto-server.netlify.app/" + NETLIFY_FUNCTIONS;
 
-const BASE_URL = "http://192.168.1.77:9000/" + NETLIFY_FUNCTIONS;
+// const BASE_URL = "http://192.168.1.77:9000/" + NETLIFY_FUNCTIONS;
 
 export const WEB_LOCAL_URL = "http://192.168.1.77:3000";
 
