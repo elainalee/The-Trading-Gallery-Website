f05b7e245e7f6df60bcdb0e032cb1211c232d33f
jennifer <jennlee0929@gmail.com>
2022-02-17 17:29:04 -0800

    fixed product detail page to have add button

--- src/components/Utils/ChooseQuantityBox.jsx
+++ src/components/Utils/ChooseQuantityBox.jsx
@@ -10,33 +10,42 @@ export default function ChooseQuantityBox(props) {
     const maxQuantity = 10;
 
     const handleMinusQuantity = () => {
-        if (quantity > 0) {
+        if (!props.addButton && quantity > 0) {
             props.handleUpdateButton(quantity - 1);
+        } else {
+            props.setQuantity(quantity - 1);
         }
     }
 
     const handlePlusQuantity = () => {
-        if (quantity < maxQuantity) {
+        if (!props.addButton && quantity < maxQuantity) {
             props.handleUpdateButton(quantity + 1);
+        } else {
+            props.setQuantity(quantity + 1);
         }
     }
 
     return (
         <div className="chooseQuantity">
-            <div className="box">
-                <div className="quantity-section">
-                    <div className="section" onClick={handleMinusQuantity}>
-                        <i className={"fas fa-minus" + (quantity <= 0 ? " disable" : "")}></i>
-                    </div>
-                    <div className="section">
-                        <text className="quantity">{quantity}</text>
-                    </div>
-                    <div className="section" onClick={handlePlusQuantity}>
-                        <i className={"fas fa-plus" + (quantity >= maxQuantity ? " disable" : "")}></i>
+            <div className={props.addButton ? "row" : ""}>
+                <div className="box">
+                    <div className="quantity-section">
+                        <div className="section" onClick={handleMinusQuantity}>
+                            <i className={"fas fa-minus" + (quantity <= 0 ? " disable" : "")}></i>
+                        </div>
+                        <div className="section">
+                            <text className="quantity">{quantity}</text>
+                        </div>
+                        <div className="section" onClick={handlePlusQuantity}>
+                            <i className={"fas fa-plus" + (quantity >= maxQuantity ? " disable" : "")}></i>
+                        </div>
                     </div>
                 </div>
+                {props.addButton
+                    ? <div className="addButton">{props.addButton}</div>
+                    : <text className="removeText" onClick={props.handleRemoveButton}>Remove</text>
+                }
             </div>
-            <text className="removeText" onClick={props.handleRemoveButton}>Remove</text>
         </div>
         
     );
